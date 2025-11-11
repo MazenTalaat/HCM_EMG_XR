5d3ede3bda2647a1e8218fdea7fd9ad7dd5ef05c
Mazen Talaat <mazen.haggag29@gmail.com>
2025-11-11 21:10:44 +0200

    001

--- Assets/Scripts/EndPoints.cs
+++ Assets/Scripts/EndPoints.cs
@@ -9,7 +9,11 @@ public class EndPoints : MonoBehaviour
 {
     // Base URL for the MVIC server (used for saving and retrieving MVIC values)
     public static string MVICServerUrl = "http://192.168.0.115:8080";
+    //public static string MVICServerUrl = "http://127.0.0.1:8080";
 
     // IP address for the Qualisys Track Manager (QTM) server (used for real-time EMG streaming)
-    public static string QTMServerIp = "192.168.0.101";
+    public static string QTMServerIp = "192.168.0.100";
+    //public static string QTMServerIp = "127.0.0.1"; 
+
+    
 }
